640a0a1265811916c0c76f005f4c8380bc7981e4
xiefei <xiefei.admin@gmail.com>
2012-10-15 13:41:24 +0800

    Initial Commit

---
+++ laoying/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  laoying
+//
+//  Created by Qing on 12-10-15.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ laoying/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  laoying
+//
+//  Created by Qing on 12-10-15.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
